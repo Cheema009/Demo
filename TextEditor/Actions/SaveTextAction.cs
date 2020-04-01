638d8919b7798cbad275769e52ef7c96f33983eb
Umer Cheema <umer.cheema2011@gmail.com>
2020-04-01 04:29:37 -0600

    master files

---
+++ TextEditor/Actions/SaveTextAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextEditor.Actions
+{
+    public class SaveTextAction
+    {
+        RichTextBox rtb;
+        string path;
+
+        public SaveTextAction(RichTextBox rTB,string path)
+        {
+            rtb = rTB;
+            this.path = path;
+        }
+
+        public void Execute()
+        {
+            StreamWriter write = new StreamWriter(File.Create(path));
+                write.Write(rtb.Text);
+                write.Dispose();
+
+            
+        }
+
+    }
+}
